1565df8204bd8d9089ef330554eca6d26e4e4f9b
yohira athenaeum <161533795+yohiraAth@users.noreply.github.com>
2024-12-11 19:35:52 +0900

    Update config.js
    add banner

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -27,10 +27,10 @@ const CONFIG = {
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
-  // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  // 青・緑・オレンジのバナーエリア
+  HEO_HERO_CATEGORY_1: { title: '利用規約', url: '/rules' },
+  HEO_HERO_CATEGORY_2: { title: 'X', url: 'https://x.com/Yohira4_TRPG' },
+  HEO_HERO_CATEGORY_3: { title: 'BlueSky', url: 'https://bsky.app/profile/yohiraathenaeum.com' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
