c418e027e3eb0181ed7abf04b77933c407d5b0c2
yohira athenaeum <161533795+yohiraAth@users.noreply.github.com>
2024-12-11 19:45:23 +0900

    Update config.js
    change card title

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,15 +15,15 @@ const CONFIG = {
   // 博客主体区左右侧组件颠倒位置
   HEO_HERO_BODY_REVERSE: false,
 
-  // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  // 左上のカードタイトル
+  HEO_HERO_TITLE_1: 'TRPGシナリオ',
+  HEO_HERO_TITLE_2: 'ツール頒布中',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '最新情報',
+  HEO_HERO_TITLE_5: 'ホームページ リリース',
+  HEO_HERO_TITLE_LINK: '',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '利用規約',
 
   // 青・緑・オレンジのバナーエリア
   HEO_HERO_CATEGORY_1: { title: '利用規約', url: '/rules' },
